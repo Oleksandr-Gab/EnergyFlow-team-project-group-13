7b4122e7ccd877b52290b1e8d19bdc7c3d11da97
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-25 02:30:02 +0200

    секція favorit 2-ї сторінки

---
+++ src/js/favoritePart.js
@@ -0,0 +1,11 @@
+
+
+const favoritePartInfo = document.querySelector('.favoritePartInfo');
+
+const savedFavorites = localStorage.getItem("favorite");
+
+
+
+const favoritePartInfoText = "<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''><p class='favoritePart-text'>It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>";
+
+favoritePartInfo.innerHTML = favoritePartInfoText;
