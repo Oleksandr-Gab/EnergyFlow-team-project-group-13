cc00b1860a026348cab10aa776e1919fbb88d00b
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-25 10:47:29 +0200

    Update favoritePart.js

--- src/js/favoritePart.js
+++ src/js/favoritePart.js
@@ -6,6 +6,4 @@ const savedFavorites = localStorage.getItem("favorite");
 
 
 
-const favoritePartInfoText = "<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''><p class='favoritePart-text'>It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>";
-
-favoritePartInfo.innerHTML = favoritePartInfoText;
+favoritePartInfo.innerHTML = "<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''><p class='favoritePart-text'>It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>";
