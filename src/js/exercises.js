54cf1694f401bca90763f6b4472c46c1dd3991f0
Serhii Krasovskyi <chokorik@gmail.com>
2024-01-26 18:44:43 +0200

    position-card

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -11,8 +11,9 @@ const limit = 8;
 // ---------------------------------------------------------
 const gallery = document.querySelector('.cards-container');
 // ---------------------------------------------------------
+const filterList = document.querySelector('.filter-list');
 const cardItem = document.querySelector('.filter-imag');
-const containerBtn = document.querySelector('.filter-button');
+const containerBtn = document.querySelector('.filter-item');
 
 const EXERCISES_URL = new URL('https://energyflow.b.goit.study/api/exercises?');
 const FILTERS_URL = new URL('https://energyflow.b.goit.study/api/filters?');
@@ -47,17 +48,31 @@ containerBtn.addEventListener('click', event => {
 
 // --------- exercises  listener-----------------------------
 
-cardItem.addEventListener('click', event => {
+gallery.addEventListener('click', event => {
   event.preventDefault();
-  console.log('hi');
+  // gallery.innerHTML = '';
+
+  gallery.classList.add('information-card');
+
+  // filterList.insertAdjacentHTML(
+  //   'afterend',
+  //   `<div class="gallery-search">
+  //           <input class="gallery-input" type="text" placeholder="Search">
+  //           <button>
+  //             <svg class="instagram-icon svg" width="12" height="12">
+  //                 <use href="./img/sprite.svg#icon-instagram"></use>
+  //               </svg>
+  //           </button>
+  //     </div>`
+  // );
+
   EXERCISES_URL.searchParams.append('limit', limit);
   EXERCISES_URL.searchParams.append('page', 1);
-  EXERCISES_URL.searchParams.append('name', event.target.name);
-  console.log(event.target);
 
   fetchExercises(EXERCISES_URL)
     .then(response => {
-      console.log(response);
+      console.log(response.results);
+      renderExercises(response.results);
     })
     .catch(error => {
       iziToast.error({
@@ -73,42 +88,41 @@ function renderExercises(arr) {
   gallery.insertAdjacentHTML(
     'afterbegin',
     arr.reduce(
-      (html, {}) =>
+      (html, { burnedCalories, name, bodyPart, rating, time, target }) =>
         html +
-        `<div class="header-card">
-                <div class="workout">WORKOUT</div>
-                <div class="rating">
-                  <p>${0.4}</p>
-                  <svg class="instagram-icon svg" width="12" height="12">
-                  <use href="./img/sprite.svg#icon-instagram"></use>
-                </svg>
-                </div>
+        `<div class="gallery-card">
+      <div class="header-card">
+        <div class="workout">WORKOUT</div>
+        <div class="rating">
+            <p>${rating}</p>
+            <svg class="instagram-icon svg" width="12" height="12">
+                <use href="./img/sprite.svg#icon-instagram"></use>
+            </svg>
+        </div>
                  
-                <div class="workout-btn-container">
-                  <button class="workout-btn">Start</button>
-                  <svg class="instagram-icon svg" width="14" height="16">
-                  <use href="./img/sprite.svg#icon-instagram"></use>
-                </svg>
-                </div>
-                
-              </div>
+        <div class="workout-btn-container">
+            <button class="workout-btn">Start</button>
+            <svg class="instagram-icon svg" width="14" height="16">
+                <use href="./img/sprite.svg#icon-instagram"></use>
+            </svg>
+        </div>
+      </div>
 
-              <div class="title-card">
-                  <div class="icon-card">
-                    <svg class="instagram-icon svg" width="14" height="16">
-                  <use href="./img/sprite.svg#icon-instagram"></use>
-                </svg>
-                  </div>
-                  <h3>${title}</h3>
-                </div>
+      <div class="title-card">
+          <div class="icon-card">
+              <svg class="run card-svg" width="24" height="24">
+                  <use href="../img/sprite.svg#run"></use>
+              </svg>
+          </div>
+          <h3>${name}</h3>
+      </div>
 
-                <ul class="params-card">
-                  <li class="calories"><span class="params-text">Burned calories:</span> ${calories}</li>
-                  <li class="body-part"><span class="params-text">Body part:</span> ${
-                    Body - part
-                  }</li>
-                  <li class="target"><span class="params-text">Target:</span> ${Target}</li>
-                </ul>`,
+      <ul class="params-card">
+            <li class="calories"><span class="params-text">Burned calories:</span> ${burnedCalories}/${time}min</li>
+            <li class="body-part"><span class="params-text">Body part:</span> ${bodyPart}</li>
+            <li class="target"><span class="params-text">Target:</span> ${target}</li>
+      </ul>
+    </div>`,
       ''
     )
   );
