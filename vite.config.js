c43a5e3ba772eb69f26c773711c6ac1fb5f50e63
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-26 18:33:50 +0200

    зміни

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig(({ command }) => {
           entryFileNames: 'commonHelpers.js',
         },
       },
-      outDir: '../dist',
+      outDir: './dist',
     },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
