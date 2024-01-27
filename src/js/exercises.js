6dca0ee561191ff93133a2ea14335a06adc2c6df
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-27 15:51:11 +0200

    a

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,3 +1,10 @@
+// для додавання 'npm install axios'
+import axios from 'axios';
+// для додавання 'npm install izitoast --save'
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+
 const FILTER_LIST = document.querySelector('.FilterList');
 let filterExercises;
 const GALLERY = document.querySelector('.Gallery');
