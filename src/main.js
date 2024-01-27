fec6d4373ecc4ba81b2da6b99eab751d8def3581
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-27 20:40:16 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // бібліотеки
 import './js/libraies.js';
 
-import './js/favoritePart';
-import './js/exercises';
-import './js/modal-form';
+// частини
+import './js/header.js';
+import './js/favoritePart.js';
+import './js/exercises.js';
+import './js/exercisesWaist.js';
+import './js/exercisesPages.js';
+
+
 
 //------------footer----------
-import './js/footer';
+import './js/footer.js';
+
+// модалки
+import './js/modal-form.js';
