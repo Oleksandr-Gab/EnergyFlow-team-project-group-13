b637e8c2bd62deab007f242c4e6b01f1bfcd3f9c
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-31 23:28:09 +0000

    Deploying to gh-pages from @ Oleksandr-Gab/EnergyFlow-team-project-group-13@9fc75e5ff8e9403f5d10144ac0dc801727367fe6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{a as g}from"./assets/modal-form-2e9a4be8.js";import"./assets/vendor-8598a644.js";const u="<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''> <p class='favoritePart-text'>It appears that you havent added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p>",e=document.querySelector(".favoritePartInfo"),s=localStorage.getItem("favoritesCard");console.log(s);let t=[];async function h(){if(s!=null){t=JSON.parse(s);try{console.log("sssss"),await p(t),g()}catch{iziToast.error({message:"Помилка, запиту. повторіть запит.",color:"red",position:"topCenter"})}}else e.innerHTML=u}async function p(a){let r=a.reduce((i,{burnedCalories:o,name:c,bodyPart:l,time:n,target:d,_id:v})=>i+`<li class="fav-gallery-card">
+import{a as u}from"./assets/modal-form-6908fd3a.js";import"./assets/vendor-8598a644.js";const g="<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''> <p class='favoritePart-text'>It appears that you havent added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p>",e=document.querySelector(".favoritePartInfo"),a=localStorage.getItem("favoritesCard");let t=[];function h(){if(a!=null){t=JSON.parse(a);try{p(t)}catch{iziToast.error({message:"Помилка, запиту. повторіть запит.",color:"red",position:"topCenter"})}}else e.innerHTML=g}async function p(s){let r=s.reduce((i,{burnedCalories:o,name:c,bodyPart:l,time:n,target:d,_id:v})=>i+`<li class="fav-gallery-card">
       <div class="header-card">
         <div class="fav-titel-card">  
           <div class="workout">WORKOUT</div>
@@ -30,5 +30,5 @@ import{a as g}from"./assets/modal-form-2e9a4be8.js";import"./assets/vendor-8598a
             <li class="body-part"><span class="params-text">Body part:</span> ${l}</li>
             <li class="target"><span class="params-text">Target:</span> ${d}</li>
       </ul>
-    </li>`,"");e.innerHTML=r}h();
+    </li>`,"");e.innerHTML=r,u()}h();
 //# sourceMappingURL=commonHelpers.js.map
