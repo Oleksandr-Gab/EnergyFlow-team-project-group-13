2a95d89c2cfc8e63ea5ddbeb02fb7e8896aa3896
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-31 22:40:45 +0000

    Deploying to gh-pages from @ Oleksandr-Gab/EnergyFlow-team-project-group-13@edc7ff7054091fb43389ba107215918eec1cce14 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{a as g}from"./assets/modal-form-494324ea.js";import"./assets/vendor-8598a644.js";const h=document.querySelector(".favoritePartInfo"),a=localStorage.getItem("favoritesCard");console.log(r);let t=[];r();async function r(){if(a!=null){t=JSON.parse(a);try{console.log("sssss"),await u(t),g()}catch{iziToast.error({message:"Помилка, запиту. повторіть запит.",color:"red",position:"topCenter"})}}else console.log("DDDDDDDDD")}async function u(s){let i=s.reduce((e,{burnedCalories:o,name:c,bodyPart:l,time:n,target:d,_id:v})=>e+`<li class="fav-gallery-card">
+import{a as g}from"./assets/modal-form-2e9a4be8.js";import"./assets/vendor-8598a644.js";const u="<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''> <p class='favoritePart-text'>It appears that you havent added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p>",e=document.querySelector(".favoritePartInfo"),s=localStorage.getItem("favoritesCard");console.log(s);let t=[];async function h(){if(s!=null){t=JSON.parse(s);try{console.log("sssss"),await p(t),g()}catch{iziToast.error({message:"Помилка, запиту. повторіть запит.",color:"red",position:"topCenter"})}}else e.innerHTML=u}async function p(a){let r=a.reduce((i,{burnedCalories:o,name:c,bodyPart:l,time:n,target:d,_id:v})=>i+`<li class="fav-gallery-card">
       <div class="header-card">
         <div class="fav-titel-card">  
           <div class="workout">WORKOUT</div>
@@ -30,5 +30,5 @@ import{a as g}from"./assets/modal-form-494324ea.js";import"./assets/vendor-8598a
             <li class="body-part"><span class="params-text">Body part:</span> ${l}</li>
             <li class="target"><span class="params-text">Target:</span> ${d}</li>
       </ul>
-    </li>`,"");h.innerHTML=i}
+    </li>`,"");e.innerHTML=r}h();
 //# sourceMappingURL=commonHelpers.js.map
