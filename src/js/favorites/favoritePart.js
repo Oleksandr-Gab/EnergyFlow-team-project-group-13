2b365e787eb33eadf68d4d39d1be48584efc4093
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-31 14:55:05 +0000

    aa

--- src/js/favorites/favoritePart.js
+++ src/js/favorites/favoritePart.js
@@ -1,19 +1,24 @@
-import { renderExercises } from '../exercisesWaist.js';
+import { renderExercises } from './js/exercisesWaist';
 
 const favoritePartInfo = document.querySelector('.favoritePartInfo');
-const savedFavorites = localStorage.getItem("favorite");
+const savedFavorites = localStorage.getItem('favoritesCard');
+const arrFavorite = [];
+
 
  const favoriteInfo = `
  '<img class="favoritePart-img" src="./img/dumbbell.svg" alt="">',
  '<p class="favoritePart-text">It appears that you havent added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>`;
 
 function saveExercises() {
-  if (!saveExercises) {
-    favoritePartInfo.innerHTML = favoriteInfo;
+  if (saveExercises != null) {
+    console.log("dddddd");
+    arrFavorite = JSON.parse(savedFavorites);
+    console.log(arrFavorite);
   } else {
-      const savingFavoritesData = JSON.parse(saveExercises);
-      console.log(savingFavoritesData);
-
+      favoritePartInfo.innerHTML = favoriteInfo;
   }
 }
 
+
+
+// saveExercises();
