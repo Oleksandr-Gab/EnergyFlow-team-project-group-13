39adf1fa0a86cf6e188a0191200359002094042e
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-31 08:58:53 +0000

    gddsf

---
+++ src/js/favorites/favorites.js
@@ -0,0 +1,14 @@
+import '../libraies.js';
+
+// частини
+import '../header.js';
+
+import '../quote-of-the-day.js';
+import './favoritePart.js';
+
+// кнопка скрол
+import '../skroll-btn.js';
+
+// модалки
+import '../modal-pop-up.js';
+import '../modal-form.js';
