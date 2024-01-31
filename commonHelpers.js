3a1fc81e5b9c2e38492f6350af7696697d11ced0
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-31 21:07:30 +0000

    Deploying to gh-pages from @ Oleksandr-Gab/EnergyFlow-team-project-group-13@a7f302d555edc1e6f10bd1cd87ec289bd4f0b34b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,34 +1,36 @@
-import{a as t}from"./assets/skroll-btn-c59c125f.js";import"./assets/vendor-8598a644.js";const e=document.querySelector(".favoritePartInfo"),a=localStorage.getItem("favoritesCard");let s=[];const p=`
+import{a as e}from"./assets/modal-form-d7111fad.js";import"./assets/vendor-8598a644.js";const r=document.querySelector(".favoritePartInfo"),a=localStorage.getItem("favoritesCard");let t=[];const u=`
  '<img class="favoritePart-img" src="./img/dumbbell.svg" alt="">',
- '<p class="favoritePart-text">It appears that you havent added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>`;h();async function h(){a!=null?(s=JSON.parse(a),await f(s),t()):e.innerHTML=p}async function f(r){let i=r.reduce((o,{burnedCalories:c,name:l,bodyPart:n,rating:d,time:v,target:u,_id:g})=>o+`<li class="gallery-card">
+ '<p class="favoritePart-text">It appears that you havent added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>`;h();async function h(){if(a!=null){t=JSON.parse(a);try{console.log("sssss"),await p(t),e()}catch{iziToast.error({message:"Помилка, запиту. повторіть запит.",color:"red",position:"topCenter"})}}else console.log("DDDDDDDDD"),r.innerHTML=u}async function p(s){let i=s.reduce((o,{burnedCalories:c,name:l,bodyPart:n,time:d,target:v,_id:g})=>o+`<li class="fav-gallery-card">
       <div class="header-card">
-        <div class="workout">WORKOUT</div>
-        <div class="rating">
-            <p>${d}</p>
-            <svg class="icon-star" width="12" height="12">
-                <use href="../../img/sprite.svg#icon-star"></use>
-            </svg>
-        </div>
+        <div class="fav-titel-card">  
+          <div class="workout">WORKOUT</div>
+            <div class="trash">
+              <svg class="icon-trash" width="16" height="16">
+                <use href="./img/sprite.svg#trash"></use>
+              </svg>
+            </div>
+          </div>
+
         <div class="workout-btn-container" data-action="right">
-            <button class="workout-btn" id="${g}">Start
+          <button class="workout-btn" id="${g}">Start
             <svg class="icon-right" width="14" height="16">
-                <use href="../img/sprite.svg#icon-right"></use>
+                <use href="./img/sprite.svg#icon-right"></use>
             </svg>
-            </button>
+          </button>
         </div>
       </div>
       <div class="title-card">
           <div class="icon-card">
               <svg class="run" width="24" height="24">
-                  <use href="../img/sprite.svg#run"></use>
+                  <use href="./img/sprite.svg#run"></use>
               </svg>
           </div>
           <h3>${l}</h3>
       </div>
       <ul class="params-card">
-            <li class="calories"><span class="params-text">Burned calories:</span> ${c}/${v}min</li>
+            <li class="calories"><span class="params-text">Burned calories:</span> ${c}/${d}min</li>
             <li class="body-part"><span class="params-text">Body part:</span> ${n}</li>
-            <li class="target"><span class="params-text">Target:</span> ${u}</li>
+            <li class="target"><span class="params-text">Target:</span> ${v}</li>
       </ul>
-    </li>`,"");e.innerHTML=i,t()}
+    </li>`,"");r.innerHTML=i,e()}
 //# sourceMappingURL=commonHelpers.js.map
