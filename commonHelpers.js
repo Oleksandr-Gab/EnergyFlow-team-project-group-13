d90a9acd198cfcaf0fabf216e91ee9a01c8969ce
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-02-01 13:29:11 +0000

    Deploying to gh-pages from @ Oleksandr-Gab/EnergyFlow-team-project-group-13@771460e1165a43b34d970bd31a4ed0f5ced8652f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import{i,a as m}from"./assets/skroll-btn-d04490aa.js";import{i as l,T as q,a as S}from"./assets/vendor-29bef8ec.js";const T="<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''> <p class='favoritePart-text'>It appears that you havent added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p>",s=document.querySelector(".favoritePartInfo"),u=localStorage.getItem("favoritesCard"),w=document.querySelector(".quote-fav-info"),g="quote-of-the-day",n="date",D=new Date,c=D.getDate();let d;async function I(){try{const a=(await S.create({baseURL:"https://energyflow.b.goit.study/api"}).get("/quote")).data;f(a),b(a,c)}catch(t){l.error({timeout:5e3,title:"Error",message:t.message,position:"topRight"})}}$();//! SAVE QUOTE AND DATA
-function b(t,e){localStorage.setItem(g,JSON.stringify(t)),localStorage.setItem(n,e)}//! RENDER QUOTE
-function f(t){const e=[t];w.innerHTML=e.reduce((a,{quote:r,author:o})=>a+`<p class="quote-text">${r}</p>
+import{i,a as m}from"./assets/skroll-btn-60c9a70b.js";import{i as l,T as q,a as S}from"./assets/vendor-29bef8ec.js";const T="<img class='favoritePart-img' src='./img/dumbbell.svg' alt=''> <p class='favoritePart-text'>It appears that you havent added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p>",s=document.querySelector(".favoritePartInfo"),d=localStorage.getItem("favoritesCard"),w=document.querySelector(".quote-fav-info"),f="quote-of-the-day",n="date",D=new Date,c=D.getDate();let g;async function I(){try{const a=(await S.create({baseURL:"https://energyflow.b.goit.study/api"}).get("/quote")).data;p(a),$(a,c)}catch(t){l.error({timeout:5e3,title:"Error",message:t.message,position:"topRight"})}}b();//! SAVE QUOTE AND DATA
+function $(t,e){localStorage.setItem(f,JSON.stringify(t)),localStorage.setItem(n,e)}//! RENDER QUOTE
+function p(t){const e=[t];w.innerHTML=e.reduce((a,{quote:r,author:o})=>a+`<p class="quote-text">${r}</p>
       <h3 class="quote-author">${o}</h3>`,"")}//! CHECK DAY AND GET NEW QUOTE
-async function $(){const t=localStorage.getItem(n);if(isNaN(t)){l.error({timeout:5e3,title:"Error",message:error.message,position:"topRight"});return}if(parseInt(t)===c){const e=localStorage.getItem(g);if(e){const a=JSON.parse(e);f(a)}return}await I(),localStorage.setItem(n,c.toString())}new q("#fan-quote",{speed:26,startDelay:300,waitUntilVisible:!0,afterComplete:function(t){t.destroy()}}).go();function A(){if(u!=null){d=JSON.parse(u);try{s.innerHTML=" ",x(d)}catch{l.error({message:"Error, query. repeat the request.",color:"red",position:"topCenter"})}}else s.insertAdjacentHTML("afterbegin",T),s.style.justifyContent="center"}async function x(t){let e=t.reduce((a,{burnedCalories:r,name:o,bodyPart:p,time:v,target:h,_id:y})=>a+`<li class="fav-gallery-card">
+async function b(){const t=localStorage.getItem(n);if(isNaN(t)){l.error({timeout:5e3,title:"Error",message:error.message,position:"topRight"});return}if(parseInt(t)===c){const e=localStorage.getItem(f);if(e){const a=JSON.parse(e);p(a)}return}await I(),localStorage.setItem(n,c.toString())}new q("#fan-quote",{speed:26,startDelay:300,waitUntilVisible:!0,afterComplete:function(t){t.destroy()}}).go();function A(){if(d!=null){g=JSON.parse(d);try{s.innerHTML=" ",x(g)}catch{l.error({message:"Error, query. repeat the request.",color:"red",position:"topCenter"})}}else s.insertAdjacentHTML("afterbegin",T),s.style.justifyContent="center"}async function x(t){let e=t.reduce((a,{burnedCalories:r,name:o,bodyPart:v,time:h,target:y,_id:u})=>a+`<li class="fav-gallery-card" id="${u}">
       <div class="header-card">
         <div class="fav-titel-card">  
           <div class="workout">WORKOUT</div>
@@ -14,7 +14,7 @@ async function $(){const t=localStorage.getItem(n);if(isNaN(t)){l.error({timeout
           </div>
 
         <div class="workout-btn-container" data-action="right">
-          <button class="workout-btn" id="${y}">Start
+          <button class="workout-btn" id="${u}">Start
             <svg class="icon-right" width="14" height="16">
                 <use href="${i}#icon-right"></use>
             </svg>
@@ -30,9 +30,9 @@ async function $(){const t=localStorage.getItem(n);if(isNaN(t)){l.error({timeout
           <h3>${o}</h3>
       </div>
       <ul class="params-card">
-            <li class="calories"><span class="params-text">Burned calories:</span> ${r}/${v}min</li>
-            <li class="body-part"><span class="params-text">Body part:</span> ${p}</li>
-            <li class="target"><span class="params-text">Target:</span> ${h}</li>
+            <li class="calories"><span class="params-text">Burned calories:</span> ${r}/${h}min</li>
+            <li class="body-part"><span class="params-text">Body part:</span> ${v}</li>
+            <li class="target"><span class="params-text">Target:</span> ${y}</li>
       </ul>
     </li>`,"");s.innerHTML=e,m()}A();
 //# sourceMappingURL=commonHelpers.js.map
