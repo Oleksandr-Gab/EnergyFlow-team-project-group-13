3a1cf2277e63017be50d31dfab935ef038235dcb
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-02-01 02:00:47 +0200

    Update modal-form.js

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -1,48 +1,48 @@
-import axios from 'axios';
-import { id } from './modals-functions';
-// для додавання 'npm install izitoast --save'
+// import axios from 'axios';
+// import { id } from './modals-functions';
+// // для додавання 'npm install izitoast --save'
 
-import { operationSuccess } from 'izitoast';
+// import { operationSuccess } from 'izitoast';
 
-const refs = {
-  modalBackdrop: document.querySelector('.js-backdrop-modal'),
-  form: document.querySelector('.js-rating-form'),
-  exerciseModal: document.querySelector('.modal'),
-  rateValue: document.querySelector('.js-rating-data'),
-  starsContainer: document.querySelector('.js-stars-list'),
-};
+// const refs = {
+//   modalBackdrop: document.querySelector('.js-backdrop-modal'),
+//   form: document.querySelector('.js-rating-form'),
+//   exerciseModal: document.querySelector('.modal'),
+//   rateValue: document.querySelector('.js-rating-data'),
+//   starsContainer: document.querySelector('.js-stars-list'),
+// };
 
-refs.form.addEventListener('submit', handleSubmit);
-refs.starsContainer.addEventListener('click', handleStarClick);
+// refs.form.addEventListener('submit', handleSubmit);
+// refs.starsContainer.addEventListener('click', handleStarClick);
 
-function handleStarClick(e) {
-  if (e.target.classList.contains('rating-label')) {
-    refs.rateValue.textContent = `${e.target.dataset.rate}.0`;
-  }
-}
+// function handleStarClick(e) {
+//   if (e.target.classList.contains('rating-label')) {
+//     refs.rateValue.textContent = `${e.target.dataset.rate}.0`;
+//   }
+// }
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  try {
-    const response = await axios.patch(
-      `https://energyflow.b.goit.study/api/exercises/${id}/rating`,
-      {
-        rate: parseInt(e.target.elements.rating.value),
-        email: e.target.elements.email.value.trim(),
-        review: e.target.elements.comment.value.trim(),
-      }
-    );
-    operationSuccess('Thank you! Your opinion really important for us!');
-  } catch (error) {
-    throw new Error(error.message);
-  } finally {
-    resetForm();
-  }
-}
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   try {
+//     const response = await axios.patch(
+//       `https://energyflow.b.goit.study/api/exercises/${id}/rating`,
+//       {
+//         rate: parseInt(e.target.elements.rating.value),
+//         email: e.target.elements.email.value.trim(),
+//         review: e.target.elements.comment.value.trim(),
+//       }
+//     );
+//     operationSuccess('Thank you! Your opinion really important for us!');
+//   } catch (error) {
+//     throw new Error(error.message);
+//   } finally {
+//     resetForm();
+//   }
+// }
 
-export function resetForm() {
-  refs.modalBackdrop.classList.remove('is-open');
-  refs.exerciseModal.classList.remove('is-open');
-  refs.rateValue.textContent = '0.0';
-  refs.form.reset();
-}
+// export function resetForm() {
+//   refs.modalBackdrop.classList.remove('is-open');
+//   refs.exerciseModal.classList.remove('is-open');
+//   refs.rateValue.textContent = '0.0';
+//   refs.form.reset();
+// }
